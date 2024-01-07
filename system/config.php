02a9faf0ce7fe243dfc2b7781d981672f509544f
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-01-07 11:35:40 +0330

    Create nvc routing

---
+++ system/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+$base_url = "http://localhost/MvcBlog/";
+$base_dir = "/MvcBlog/";
+
+$tmp = explode('?' , $_SERVER['REQUEST_URI']);
+$current_route = str_replace($base_dir,'', $tmp[0]);
+
+unset($tmp);
--- system/router/Routing.php
+++ system/router/Routing.php
@@ -1,8 +1,45 @@
-<?php 
+<?php
 
 namespace System\router;
 
-class Routing {
-    
+use ReflectionMethod;
 
+class Routing
+{
+    private $current_route;
+
+    public function __construct()
+    {
+        global $current_route;
+        $this->current_route = explode('/', $current_route);
+    }
+
+    public function run()
+    {
+
+        $path = realpath(dirname(__FILE__), '/../../application/controller', $this->current_route[0] . 'php');
+        if (!file_exists($path)) {
+            echo 'Error 404 (this page is not found)';
+            exit;
+        }
+
+        require_once($path);
+
+        sizeof($this->current_route) == 1 ? $method = "index" : $method = $this->current_route[1];
+
+        $class = "Application\controllers\\" . $this->current_route[0];
+        $object = new $class;
+
+        if (method_exists($object, $method)) {
+            $reflection = new ReflectionMethod($class, $method);
+            $parameterCount = $reflection->getNumberOfParameters();
+            if ($parameterCount <= count(array_slice($this->current_route, 2))) {
+                call_user_func_array(array($object, $method), array_slice($this->current_route, 2));
+            } else {
+                echo '404 -- Parameter Error';
+            }
+        } else {
+            echo '404 -- method not exist';
+        }
+    }
 }
